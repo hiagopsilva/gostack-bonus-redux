ef98737f3cc4b81cad65c6e6e1afb49a80cd60e1
hiagopsilva <hiagohps360@gmail.com>
2020-09-08 14:36:03 -0300

    feat(sagas): n
    sagas

---
+++ src/store/modules/cart/sagas.ts
@@ -0,0 +1,47 @@
+import { all, takeLatest, select, call, put } from 'redux-saga/effects';
+import { AxiosResponse } from 'axios';
+
+import {
+  addProductToCartRequest,
+  addProductToCartSuccess,
+  addProductToCartFailure,
+} from './actions';
+import { IState } from '../..';
+import api from '../../../services/api';
+import { ActionTypes } from './types';
+
+type checkProductStockRequest = ReturnType<typeof addProductToCartRequest>;
+
+interface IStockResponse {
+  id: number;
+  quantity: number;
+}
+function* checkProductStock({ payload }: checkProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find(item => item.product.id === product.id)?.quantity ??
+      0
+    );
+  });
+
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(
+    api.get,
+    `stock/${product.id}`,
+  );
+
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
+
+  console.log(currentQuantity);
+
+  console.log('Adicionou ao carrinho');
+}
+
+export default all([
+  takeLatest(ActionTypes.addProductToCartRequest, checkProductStock),
+]);
